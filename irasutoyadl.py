93c83dcd4267f564fb18c5ab9545c4c356853862
IKE1213 <ikedakoukun1213@gmail.com>
2025-12-18 16:22:20 +0900

    個人実習-フォルダ作成から画像DLまでの関数化

--- irasutoyadl.py
+++ irasutoyadl.py
@@ -2,14 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 from pathlib import Path
 
-
 load_url = "https://www.irasutoya.com/2020/09/blog-post_386.html"
+
+
 # html = requests.get(load_url)
 # soup = BeautifulSoup(html.content, "html.parser")
 # entry = soup.find(class_="entry")
 # img = entry.find("img")
 # src = img.get("src")
 # print(src)
+
+# #画像保存先フォルダの作成
+# out_folder = Path("download_work")
+# out_folder.mkdir(exist_ok=True)
+# #画像の取得処理
+# imgdata = requests.get(getImageUrl(load_url))
+# filename = getImageUrl(load_url).split("/")[-1]
+# out_path = out_folder.joinpath(filename)
+#
+# #画像DLフォルダを書き込みモードで開く
+# f = open(out_path, "wb")
+# #画像データの書き込み
+# f.write(imgdata.content)
+# #ファイルを閉じて保存している
+# f.close()
 
 
 def getImageUrl(load_url):
@@ -21,17 +37,18 @@ def getImageUrl(load_url):
     return src
 print(getImageUrl(load_url))
 
-#画像保存先フォルダの作成
-out_folder = Path("download_work")
-out_folder.mkdir(exist_ok=True)
-#画像の取得処理
-imgdata = requests.get(getImageUrl(load_url))
-filename = getImageUrl(load_url).split("/")[-1]
-out_path = out_folder.joinpath(filename)
+#関数getImageUrlの戻り値を代入
+img_URL = getImageUrl(load_url)
+#↓代入した値を引数にした。
+def downloadImage(img_URL):
+    out_folder = Path("download_work")
+    out_folder.mkdir(exist_ok=True)
+    imgdata = requests.get(getImageUrl(load_url))
+    filename = getImageUrl(load_url).split("/")[-1]
+    out_path = out_folder.joinpath(filename)
+    f = open(out_path, "wb")
+    f.write(imgdata.content)
+    f.close()
 
-#画像DLフォルダを書き込みモードで開く
-f = open(out_path, "wb")
-#画像データの書き込み
-f.write(imgdata.content)
-#ファイルを閉じて保存している
-f.close()
+#関数downloadImage実行
+downloadImage(img_URL)
