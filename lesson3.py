42fc42815af3371152110e919732673e9e74fb03
IKE1213 <ikedakoukun1213@gmail.com>
2026-01-08 16:28:19 +0900

    いらすとや「野球」のイラストの全ページのURLをコンソールに表示

--- lesson3.py
+++ lesson3.py
@@ -2,22 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-resultUrl = "https://www.irasutoya.com/search?q=%E9%87%8E%E7%90%83&updated-max=2019-06-24T15:00:00%2B09:00&max-results=20&start=0&by-date=false"
+def get_article_urls(start_url):
+    urls = set()
+    current_url = start_url
 
-urls = []
+    while current_url:
+        resp = requests.get(current_url)
+        soup = BeautifulSoup(resp.content, "html.parser")
 
-html = requests.get(resultUrl)
-soup = BeautifulSoup(html.content, "html.parser")
-urls = list(set(urls))
+        for link in soup.find_all("a"):
+            href = link.get("href")
+            if not href:
+                continue
+            if "irasutoya.com/" in href and "blog-post" in href and "#" not in href:
+                urls.add(href)
 
-for link in soup.find_all("a"):
-    href = link.get("href")
-    if href is None:
-        continue
-    if "irasutoya.com/" in href and "blog-post" in href and "#" not in href:
-        urls.append(href)
+        next_link = soup.find("a", class_="blog-pager-older-link")
+        if next_link:
+            current_url = urljoin(current_url, next_link.get("href"))
+        else:
+            break
 
+    return list(urls)
 
-
-for u in urls:
-    print(u)
+if __name__ == "__main__":
+    resultUrl = "https://www.irasutoya.com/search?q=%E9%87%8E%E7%90%83&updated-max=2019-06-24T15:00:00%2B09:00&max-results=20&start=0&by-date=false"
+    urls = get_article_urls(resultUrl)
+    for u in urls:
+        print(u)
