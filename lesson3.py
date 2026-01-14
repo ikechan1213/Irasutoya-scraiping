e78cdf295cc6fa93153976b962ddc8a7afc56f74
IKE1213 <ikedakoukun1213@gmail.com>
2026-01-14 09:55:39 +0900

    プログラムの関数化成功

--- lesson3.py
+++ lesson3.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-def get_article_urls(start_url):
-    urls = set()
-    current_url = start_url
+def readResultHtml(targetUrl, urls):
+
+    current_url = targetUrl
 
     while current_url:
         resp = requests.get(current_url)
@@ -27,6 +27,7 @@ def get_article_urls(start_url):
 
 if __name__ == "__main__":
     resultUrl = "https://www.irasutoya.com/search?q=%E9%87%8E%E7%90%83&updated-max=2019-06-24T15:00:00%2B09:00&max-results=20&start=0&by-date=false"
-    urls = get_article_urls(resultUrl)
-    for u in urls:
+    urls = set()
+    urls_list = readResultHtml(resultUrl, urls)
+    for u in urls_list:
         print(u)
